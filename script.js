fe82d2e9835f821ee936409cf84e2531fb816649
Shubhankar-creation <shubhankarsharma0135@gmail.com>
2025-02-05 00:18:15 +0530

    shubhankar test

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ const allAudio = [new Audio('../audio/intro.wav')]
 const audioSource = document.getElementById("AudioSource");
 
 //link road // carter road //     // main
-var invalidLocations = [[72.83317652944778, 19.064512598679855], [72.82183514931508, 19.059149582832017], [72.82217957369458, 19.04654194640622], [72.83441586635318, 19.060396215930364]]
+//var invalidLocations = [[72.83317652944778, 19.064512598679855], [72.82183514931508, 19.059149582832017], [72.82217957369458, 19.04654194640622], [72.83441586635318, 19.060396215930364]]
+var invalidLocations = [[72.91751514622162, 19.081023047447346], [72.91797280170951, 19.081750954335934]] // [72.91898856670213, 19.08274711957367]
 
 var calledIntro = false;
 
@@ -70,7 +71,7 @@ mainMarkerIcon.style.height = `60px`;
 mainMarkerIcon.style.backgroundSize = '100%';
 
 var locationDiv = []
-for(let i = 0; i < 4; i++)
+for(let i = 0; i < 2; i++)
 {
     locationMarker = document.createElement('div');
     locationMarker.className = 'marker';
@@ -87,40 +88,40 @@ const currentUserMarker = new mapboxgl.Marker(mainMarkerIcon)
 .addTo(map);
     
 const mainMarker = new mapboxgl.Marker(locationDiv[0])
-.setLngLat([72.83441586635318, 19.060396215930364]) // main
+.setLngLat(invalidLocations[0]) // main
 .addTo(map);
 
 locationDiv[0].addEventListener('click', () => {
-    directions.setDestination([72.83441586635318, 19.060396215930364]);
+    directions.setDestination(invalidLocations[0]);
     DisableMarkers(0);
 });
 
 const mountMaryMarker = new mapboxgl.Marker(locationDiv[1])
-.setLngLat([72.82217957369458, 19.04654194640622]) // Marker in Mount Mary
+.setLngLat(invalidLocations[1]) // Marker in Mount Mary
 .addTo(map);
 
 locationDiv[1].addEventListener('click', () => {
     DisableMarkers(1);
-    directions.setDestination([72.82217957369458, 19.04654194640622]);
+    directions.setDestination(invalidLocations[1]);
 });
 
-const linkingRoadMarker = new mapboxgl.Marker(locationDiv[2])
-.setLngLat([72.83317652944778, 19.064512598679855]) // Marker in linking road
-.addTo(map);
+// const linkingRoadMarker = new mapboxgl.Marker(locationDiv[2])
+// .setLngLat([72.83317652944778, 19.064512598679855]) // Marker in linking road
+// .addTo(map);
 
-locationDiv[2].addEventListener('click', () => {
-    DisableMarkers(2);
-    directions.setDestination([72.83317652944778, 19.064512598679855]);
-});
+// locationDiv[2].addEventListener('click', () => {
+//     DisableMarkers(2);
+//     directions.setDestination([72.83317652944778, 19.064512598679855]);
+// });
 
-const carterRoadMarker = new mapboxgl.Marker(locationDiv[3])
-.setLngLat([72.82183514931508, 19.059149582832017]) // Marker in carter road
-.addTo(map);
+// const carterRoadMarker = new mapboxgl.Marker(locationDiv[3])
+// .setLngLat([72.82183514931508, 19.059149582832017]) // Marker in carter road
+// .addTo(map);
 
-locationDiv[3].addEventListener('click', () => {
-    DisableMarkers(3);
-    directions.setDestination([72.82183514931508, 19.059149582832017]);
-});
+// locationDiv[3].addEventListener('click', () => {
+//     DisableMarkers(3);
+//     directions.setDestination([72.82183514931508, 19.059149582832017]);
+// });
 
 function removeDefaultMarkers() {
     console.log("called on clock");
@@ -257,7 +258,7 @@ function hasReached(currentLat, currentLng)
         if(currDistance < 10)
         {
             locationReached = false;
-            if(location === invalidLocations[3])
+            if(location === invalidLocations[0])
             {
                 console.log("You have reached the main target location!");
                 document.getElementById("CorrectLocationModal").style.display = 'block';
@@ -288,6 +289,7 @@ function hasReached(currentLat, currentLng)
             }
 
             invalidLocations.pop(location);
+            console.log(invalidLocations.length + " lenght after removal");
             return true;
         }
         else 
@@ -344,7 +346,7 @@ function hasReached(currentLat, currentLng)
 
 function ClosureToStore(currentLat, currentLng) 
 {
-    var currDistance = checkProximity([currentLat, currentLng], invalidLocations[3]);
+    var currDistance = checkProximity([currentLat, currentLng], invalidLocations[0]);
 
     if(closestDistance >= currDistance)
     {
@@ -359,8 +361,9 @@ function ClosureToStore(currentLat, currentLng)
         }, 15000);
         PlayAudio("../audio/hot.wav");
     }
-    else if(closestDistance <= currDistance && currDistance <= closestDistance + 20 && enteredHot)
+    else if(closestDistance <= currDistance && currDistance <= 80 && enteredHot)
     {
+        closestDistance = currDistance;
         hotcold.style.display = 'block';
         hotcold.children[0].classList.remove('Hot');
         hotcold.children[0].classList.add('Cold');
@@ -372,6 +375,7 @@ function ClosureToStore(currentLat, currentLng)
     }
     else
     {
+        canHotCold = false;
         enteredHot = false;
         closestDistance = 40;
         hotcold.style.display = 'none';
@@ -432,7 +436,6 @@ function EnableMarkers()
             locationDiv[i].style.display = 'block';
     }
     directions.removeRoutes();
-
 }
 document.getElementsByClassName('retryBtn')[0].addEventListener('click', () => {
     EnableMarkers();
